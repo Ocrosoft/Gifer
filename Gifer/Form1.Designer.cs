f0cf99cdd3e95bc2fc1b9e20f2f314d4ab786053
Ocrosoft <ocrosoft@ocrosoft.com>
2017-06-21 11:48:26 +0800

    首次提交；

---
+++ Gifer/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Gifer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(460, 220);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "删除选择项";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 238);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "清空列表";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 295);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(460, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(367, 238);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 51);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "开始转换";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "输出文件夹：";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(265, 238);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "选择输出位置";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "压缩文件(*.zip)|*.zip";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.RestoreDirectory = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(321, 267);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 21);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 330);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Gifer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Gifer/Form1.cs
@@ -0,0 +1,219 @@
+﻿using Gif.Components;
+using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Gifer
+{
+    public partial class Form1 : Form
+    {
+        public static Random rd = new Random();
+        public string dir = "";
+        public string tmp = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var files = openFileDialog1.FileNames;
+                foreach (var s in files)
+                    listBox1.Items.Add(s);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                var index = listBox1.SelectedIndex - 1;
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                if (index >= 0) listBox1.SelectedIndex = index;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = "输出文件夹：" + folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        public bool UnZip(string fileToUnZip, string zipedFolder, string password)
+        {
+            bool result = true;
+            FileStream fs = null;
+            ZipInputStream zipStream = null;
+            ZipEntry ent = null;
+            string fileName;
+
+            if (!File.Exists(fileToUnZip))
+                return false;
+
+            if (!Directory.Exists(zipedFolder))
+                Directory.CreateDirectory(zipedFolder);
+
+            try
+            {
+                zipStream = new ZipInputStream(File.OpenRead(fileToUnZip));
+                if (!string.IsNullOrEmpty(password)) zipStream.Password = password;
+                while ((ent = zipStream.GetNextEntry()) != null)
+                {
+                    if (!string.IsNullOrEmpty(ent.Name))
+                    {
+                        fileName = Path.Combine(zipedFolder, ent.Name);
+                        fileName = fileName.Replace('/', '\\');//change by Mr.HopeGi   
+
+                        if (fileName.EndsWith("\\"))
+                        {
+                            Directory.CreateDirectory(fileName);
+                            continue;
+                        }
+
+                        fs = File.Create(fileName);
+                        int size = 2048;
+                        byte[] data = new byte[size];
+                        while (true)
+                        {
+                            size = zipStream.Read(data, 0, data.Length);
+                            if (size > 0)
+                                fs.Write(data, 0, data.Length);
+                            else
+                                break;
+                        }
+                        if (fs != null)
+                        {
+                            fs.Close();
+                            fs.Dispose();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                /*if (fs != null)
+                {
+                    fs.Close();
+                    fs.Dispose();
+                }*/
+                if (zipStream != null)
+                {
+                    zipStream.Close();
+                    zipStream.Dispose();
+                }
+                if (ent != null)
+                {
+                    ent = null;
+                }
+                GC.Collect();
+                GC.Collect(1);
+            }
+            return result;
+        }
+
+        public bool UnZip(string fileToUnZip, string zipedFolder)
+        {
+            bool result = UnZip(fileToUnZip, zipedFolder, null);
+            return result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 0)
+            {
+                MessageBox.Show("没有添加任何文件");
+                return;
+            }
+            if (label1.Text.Substring(6).Length <= 0)
+            {
+                MessageBox.Show("输出路径为空");
+                return;
+            }
+            foreach (var item in listBox1.Items)
+            {
+                dir = rdPath();
+                try
+                {
+                    if(UnZip(item.ToString(), Path.GetTempPath() + dir))
+                    {
+                        try
+                        {
+                            tmp = item.ToString();
+                            tmp = tmp.Substring(tmp.LastIndexOf("\\") + 1);
+                            tmp = tmp.Substring(0, tmp.IndexOf('.') - 1) + ".gif";
+                            ToGif(Path.GetTempPath() + dir, label1.Text.Substring(6) + "\\" + tmp, Int32.Parse(textBox1.Text), true);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("转换失败,跳过该文件");
+                            Directory.Delete(Path.GetTempPath() + dir, true);
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("解压失败,跳过该文件");
+                        Directory.Delete(Path.GetTempPath() + dir, true);
+                        continue;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("解压失败,跳过该文件");
+                    Directory.Delete(Path.GetTempPath() + dir, true);
+                    continue;
+                }
+                Directory.Delete(Path.GetTempPath() + dir, true);
+            }
+            MessageBox.Show("转换结束");
+        }
+
+        public void ToGif(string directory, string giffile, int time, bool repeat)
+        {
+            string[] files = Directory.GetFileSystemEntries(directory, "*.jpg");
+
+            AnimatedGifEncoder e = new AnimatedGifEncoder();
+            e.Start(giffile);
+            e.SetDelay(time);
+            e.SetRepeat(repeat ? 0 : -1);
+            for (int i = 0, count = files.Length; i < count; i++)
+            {
+                var img = Image.FromFile(files[i]);
+                e.AddFrame(img);
+                img.Dispose();
+                progressBar1.Value = (int)(i * 1.0 / files.Length * 100);
+                Application.DoEvents();
+            }
+            e.Finish();
+            progressBar1.Value = 0;
+        }
+
+        public string rdPath()
+        {
+            string ret = "";
+            for (int i = 0; i < 6; i++)
+            {
+                int tmp = rd.Next(36);
+                if (tmp >= 10) ret += (char)((int)'a' + (tmp - 10));
+                else ret += tmp;
+            }
+            if (Directory.Exists(Path.GetTempPath() + ret)) return rdPath();
+            else return ret;
+        }
+    }
+}
